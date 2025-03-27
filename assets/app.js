b9ac540932897d88ad5151f29da92091c958597d
Moises Alfaro <107770920+MoisesAlfar0o@users.noreply.github.com>
2025-03-27 13:41:55 -0600

    Add files via upload

--- assets/app.js
+++ assets/app.js
@@ -47,7 +47,8 @@ function validateYear(year) {
         errorYear.textContent = 'Must be >= 1900'; // Show error message
         return false;
     }
-    if (year => currentYear) {
+    if (year >= currentYear) {
+        console.log(currentYear)
         errorYear.textContent = 'Must be in the past'; // Show error message if future year
         return false;
     }
